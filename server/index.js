b15ea91d9b627bb261df419c548897c73864808b
MohdZaidFrontend <za752911@mail.com>
2022-10-28 10:55:47 +0530

    third commit

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ app.use("/api/user", userRoutes);
 //   app.use(express.static(path.join(__dirname,"./client/build")));
   app.get("/", (req, res) => {
     res.send("Hello!")
-
+  })
 
  
 app.listen(port, () => {
