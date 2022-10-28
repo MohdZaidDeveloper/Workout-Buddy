f8939a1ccea1744fcac02711e9eab571ae32352c
MohdZaidFrontend <za752911@mail.com>
2022-10-28 10:55:21 +0530

    third commit

--- server/index.js
+++ server/index.js
@@ -24,15 +24,14 @@ app.use(cors());
 app.use("/api/workouts", workoutRoutes);
 app.use("/api/user", userRoutes);
 
-//Deployment
+// //Deployment
 
 
 
 
-  app.use(express.static(path.join(__dirname,"./client/build")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname,"./client/build/index.html"));
-  });
+//   app.use(express.static(path.join(__dirname,"./client/build")));
+  app.get("/", (req, res) => {
+    res.send("Hello!")
 
 
  
